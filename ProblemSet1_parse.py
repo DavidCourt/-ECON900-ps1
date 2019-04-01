635705e5b2318dd5743470c7ba3cbab09c3ec4b8
David C <dcourtr@clemson.edu>
2019-03-31 20:12:00 -0400

    expanded to all rows but not bugfree

--- ProblemSet1_parse.py
+++ ProblemSet1_parse.py
@@ -16,48 +16,35 @@ for one_file_name in glob.glob("html_files/*.html"):
 	soup = BeautifulSoup(f.read(), 'html.parser')
 	f.close()
 	game_table=soup.find("table", {"class": "collection_table"})
-	# game_row=game_table.find_all("tr", {"id": "row_"})
-	# for r in game_row:
-	# 	game_title=r.find("td", {"id": "CEcell_objectname1"}).find("div", {"id": "results_objectname1"}).find("a").text
-	# 	print(game_title)
-	# 	game_cell=r.find_all("td", {"class": "collection_bggrating"})
-	# 	#print(game_cell)
-	# 	for c in game_cell:
-	# 		game_geekrating=c.text
-	# 		print(game_geekrating)
-		# game_geekrating=r.find("td", {"class": "collection_bggrating"}).text
-		# print(game_geekrating)
-		# game_avgrating=r.find("td", {"class": "collection_bggrating"}).text
-		# print(game_avgrating)
-		# game_votenum=r.find("td", {"class": "collection_bggrating"}).text
-		# print(game_votenum)
-		# game_price=r.find("td", {"class": "collection_shop"}).find("div", {"class": "aad"}).text
-		# print(game_price)
-	game_row=game_table.find("tr", {"id": "row_"})	
-	game_cell=game_row.find_all("td", {"class": "collection_bggrating"})
-	i=0
-	for c in game_cell:
-		if i == 0:
-			game_geekrating=c.text
-		elif i == 1:
-			game_avgrating=c.text
-		elif i == 2:
-			game_votenum=c.text
+	game_row=game_table.find_all("tr", {"id": "row_"})
+	for r in game_row:
+		game_title=r.find("td", {"id": "CEcell_objectname1"}).find("div", {"id": "results_objectname1"}).find("a").text
+		print(game_title)
+		game_cell=r.find_all("td", {"class": "collection_bggrating"})
+		i=0
+		for c in game_cell:
+			if i == 0:
+				game_geekrating=c.text
+			elif i == 1:
+				game_avgrating=c.text
+			elif i == 2:
+				game_votenum=c.text
 
-		i=i+1	
-	print("rating" + game_geekrating)
-	print("avg" + game_avgrating)
-	print("vote" + game_votenum)
+			i=i+1	
+		print("rating" + game_geekrating)
+		print("avg" + game_avgrating)
+		print("vote" + game_votenum)
+		game_price=r.find("td", {"class": "collection_shop"}).find("div", {"class": "aad"}).text
+		print(game_price)
 
-		
-		# df=df.append({
-		# 	'scrapping_time': scrapping_time,
-		# 	'title': game_title,
-		# 	'geek_rating': game_geekrating,
-		# 	'avg_rating': game_avgrating,
-		# 	'vote_number': game_votenum,
-		# 	'list_price': game_price
-		# 	}, ignore_index=True)
+		df=df.append({
+			'scrapping_time': scrapping_time,
+			'title': game_title,
+			'geek_rating': game_geekrating,
+			'avg_rating': game_avgrating,
+			'vote_number': game_votenum,
+			'list_price': game_price
+			}, ignore_index=True)
 
 print(df)
 
